a8217b7794c80bd3d7ede35e782297d90e8f9e6f
Ludmyla Oliveira Soares <49792306+ludmylasoares@users.noreply.github.com>
2019-04-21 22:08:51 -0300

    Add files via upload

---
+++ 01_EstruturaSequencial/01_alo_mundo.py
@@ -0,0 +1,6 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+print ('Alô, mundo!')
+Alô, mundo!
+>>> 
