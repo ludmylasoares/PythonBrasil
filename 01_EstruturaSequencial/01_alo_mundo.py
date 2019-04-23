2e8344e3fcd8443dd3c991d68eb771402bab1a1b
Ludmyla Oliveira Soares <49792306+ludmylasoares@users.noreply.github.com>
2019-04-23 13:20:19 -0300

    Update 01_alo_mundo.py

--- 01_EstruturaSequencial/01_alo_mundo.py
+++ 01_EstruturaSequencial/01_alo_mundo.py
@@ -1,4 +1,4 @@
-Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Python 3.6.2 (v3.6.2:5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
 >>> 
 print ('Alô, mundo!')
 Alô, mundo!
