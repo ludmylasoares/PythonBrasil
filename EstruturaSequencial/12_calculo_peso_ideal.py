6981a4863ce1fdfa4d896a40586b97388dde0a46
Ludmyla Oliveira Soares <49792306+ludmylasoares@users.noreply.github.com>
2019-05-07 19:50:25 -0300

    Update 12_calculo_peso_ideal.py

--- EstruturaSequencial/12_calculo_peso_ideal.py
+++ EstruturaSequencial/12_calculo_peso_ideal.py
@@ -1,4 +1,5 @@
 '''
+ Autora: Ludmyla Oliveira Soares
  Data: 28/04/19
  12. Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a 
     seguinte fórmula: (72.7*altura) - 58
