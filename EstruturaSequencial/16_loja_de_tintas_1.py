9e440c00f04e3a35dd5c2e8ab85c3baa196350ba
Ludmyla Oliveira Soares <49792306+ludmylasoares@users.noreply.github.com>
2019-05-07 19:31:56 -0300

    Update 16_loja_de_tintas_1.py

--- EstruturaSequencial/16_loja_de_tintas_1.py
+++ EstruturaSequencial/16_loja_de_tintas_1.py
@@ -1,7 +1,6 @@
 
 '''
- Autora: Ludmyla Oliveira Soares
- Data: 05/05/19
+ Data: 06/05/19
  16. Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros 
  quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 
  3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. 
