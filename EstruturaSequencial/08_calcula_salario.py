6744a35dc18a6a2bc60279e6555d2760b77df734
Ludmyla Oliveira Soares <49792306+ludmylasoares@users.noreply.github.com>
2019-05-07 19:42:21 -0300

    Update 08_calcula_salario.py

--- EstruturaSequencial/08_calcula_salario.py
+++ EstruturaSequencial/08_calcula_salario.py
@@ -1,5 +1,4 @@
 '''
- Autora: Ludmyla Oliveira Soares
  Data: 27/04/19
  08. Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o 
  total do seu salário no referido mês.
